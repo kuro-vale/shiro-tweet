10ab6d650036256323eaffcb8df60e69e0177bbe
kuro-vale <jsalcedo218@gmail.com>
2023-10-06 22:34:19 -0500

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+        screens: {
+            'md': {'max': '1000px'},
+            'sm': {'max': '600px'},
+        }
+    },
+    plugins: [],
+}
+
