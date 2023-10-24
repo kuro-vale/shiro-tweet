e7351a1d87ff29d2ab34f20649de358878a85ff2
kuro-vale <jsalcedo218@gmail.com>
2023-10-23 22:57:41 -0500

    Add home tabs

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,28 @@
 import TweetList from "../components/tweets/tweet-list";
 import TimelineLayout from "../components/layouts/timeline-layout";
 import Aside from "../components/menus/aside";
+import type {TabsProps} from "antd";
+import {Tabs, Typography} from "antd";
+
+const {Text} = Typography;
 
 function Home() {
+  const items: TabsProps["items"] = [{
+    key: "1",
+    label: "For you",
+    disabled: true,
+  }, {
+    key: "2",
+    label: "Following",
+  }];
+
   return (
     <>
       <TimelineLayout>
+        <div className="h-[53px] flex items-center">
+          <Text strong className="text-white text-xl ml-4">Home</Text>
+        </div>
+        <Tabs defaultActiveKey="2" items={items}/>
         <TweetList/>
       </TimelineLayout>
       <Aside showSearchBar={true}/>
